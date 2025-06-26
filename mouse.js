554a603cc63e49cedb2de7968306d0c4f66bd2bd
bdd88 <90480187+bdd88@users.noreply.github.com>
2025-06-26 18:22:46 -0500

    Added tracking for click and drag

--- mouse.js
+++ mouse.js
@@ -4,6 +4,12 @@ export class mouse {
     clickHeld = false;
     canvasX;
     canvasY;
+    x1;
+    y1;
+    pressTime;
+    x2;
+    y2;
+    dragTime;
 
     constructor(canvas) {
         this.canvasX = canvas.getBoundingClientRect().left;
@@ -12,10 +18,17 @@ export class mouse {
 
     press(e) {
         this.clickHeld = true;
+        this.x1 = this.x;
+        this.y1 = this.y;
+        this.pressTime = Date.now();
     }
 
     release(e) {
         this.clickHeld = false;
+        this.x2 = this.x;
+        this.y2 = this.y;
+        this.dragTime = Date.now() - this.pressTime;
+        this.pressTime = null;
     }
 
     move(e) {
