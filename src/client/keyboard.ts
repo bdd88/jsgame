9310c01b30d21da3d07f6e4de134eb1c00e7f7b5
bdd88 <90480187+bdd88@users.noreply.github.com>
2025-07-15 15:34:58 -0500

    TS fixes for keyboard

--- src/client/keyboard.ts
+++ src/client/keyboard.ts
@@ -1,7 +1,7 @@
 export class keyboard {
-    heldKeys;
-    callbackOnPress;
-    callbackOnRelease;
+    heldKeys: Set<string>;
+    callbackOnPress: Map<string,Function>;
+    callbackOnRelease: Map<string,Function>;
 
     constructor() {
         this.heldKeys = new Set();
@@ -9,17 +9,19 @@ export class keyboard {
         this.callbackOnRelease = new Map();
     }
 
-    press(e) {
+    press(e: KeyboardEvent) {
         if (this.callbackOnPress.has(e.key)) {
-            const callback = this.callbackOnPress.get(e.key);
+            if (this.callbackOnPress.get(e.key) === undefined) { return false; }
+            const callback = this.callbackOnPress.get(e.key) as Function;
             callback();
         }
         this.heldKeys.add(e.key);
     }
 
-    release(e) {
+    release(e: KeyboardEvent) {
         if (this.callbackOnRelease.has(e.key)) {
-            const callback = this.callbackOnRelease.get(e.key);
+            if (this.callbackOnRelease.get(e.key) === undefined) { return false; }
+            const callback = this.callbackOnRelease.get(e.key) as Function;
             callback();
         }
         this.heldKeys.delete(e.key);
