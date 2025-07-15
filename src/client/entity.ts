46eb941905b8eb59a1cfd9444280d0984803b496
bdd88 <90480187+bdd88@users.noreply.github.com>
2025-07-15 15:13:09 -0500

    TS fixes for entity class

--- src/client/entity.ts
+++ src/client/entity.ts
@@ -4,6 +4,8 @@ import { physicsObject } from "./physics";
  * An in-game object that has physics simulations applied to it.
  */
 export class entity extends physicsObject {
-    name = "Entity"
-    description = "Generic description of the entity."
+    name = "Entity";
+    description = "Generic description of the entity.";
+    color = "White";
+    opacity = 100;
 }
