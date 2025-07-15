e8584a20ea82850d79cd98546f70faaf454ba0e3
bdd88 <90480187+bdd88@users.noreply.github.com>
2025-07-15 12:48:31 -0500

    Typescript fixes for control class

--- src/client/controls.ts
+++ src/client/controls.ts
@@ -4,9 +4,10 @@ import { entities } from "./entities";
 import { sand } from './matter/granule/sand';
 
 export class controls {
-    mouse;
-    keyboard;
-    paused = false;
+    mouse: mouse;
+    keyboard: keyboard;
+    entities: entities;
+    paused: boolean = false;
 
     /**
      * 
@@ -14,7 +15,7 @@ export class controls {
      * @param {keyboard} keyboard
      * @param {entities} entities
      */
-    constructor (mouse, keyboard, entities) {
+    constructor (mouse: mouse, keyboard: keyboard, entities: entities) {
         this.mouse = mouse;
         this.keyboard = keyboard;
         this.entities = entities
@@ -27,8 +28,8 @@ export class controls {
         document.addEventListener('keyup', (e) => this.keyboard.release(e));
 
         this.keyboard.callbackOnPress.set('Escape', this.togglePause);
-        this.mouse.addButtonPressCallback('Left', this.mouseInteractions);
-        this.mouse.addButtonPressCallback('Right', this.togglePause);
+        this.mouse.addButtonPressCallback('Left', this.mouseInteractions, []);
+        this.mouse.addButtonPressCallback('Right', this.togglePause, []);
     }
 
     togglePause = () => {
